6f29d7ea2dd44f9c14ecb8789678de3177e11705
niramay447 <nk447@snu.edu.in>
2024-10-26 11:54:49 +0530

    Checkpoint 5.5

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -16,7 +17,10 @@ class User(UserMixin, db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64),index=True,unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+    
     # posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
     posts: so.WriteOnlyMapped['Post'] = so.relationship('Post', back_populates='author')
 
@@ -27,6 +31,11 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     def __repr__(self) -> str:
         return '<User {}>'.format(self.username)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+    
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
