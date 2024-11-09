af96161e717ce43ebd7aced427faa17160493692
niramay447 <nk447@snu.edu.in>
2024-11-09 12:03:50 +0530

    Paginate Explore and index pages

--- app/routes.py
+++ app/routes.py
@@ -24,16 +24,16 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
 
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template('index.html', title='Home',form=form, posts=posts)
-
-# @app.route('/login', methods=['GET','POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         flash('Login requested for user{},remember_me={}'.format(form.username.data,form.remember_me.data))
-#         return redirect(url_for('index'))
-#     return render_template('login.html',title='Sign In',form=form)
+    page = request.args.get('page',1,type=int)
+    posts = db.paginate(current_user.following_posts(),page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],error_out=False)
+    
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    
+    return render_template('index.html', title='Home',form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -137,14 +137,20 @@ def unfollow(username):
         current_user.unfollow(user)
         db.session.commit()
         flash(f'You are not following {username}.')
-        return redirect(url_for('user',username))
+        return redirect(url_for('user',username=username))
     else:
         return redirect(url_for('index'))
     
-@app.route('explore')
+@app.route('/explore')
 @login_required
 def explore():
-    query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
 
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page',1,type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'],error_out = False)
+    
+    next_url = url_for('index' , page = posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page = posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['']
+    POSTS_PER_PAGE = 3
